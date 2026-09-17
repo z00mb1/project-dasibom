f3d5e4af05612aca64df40c1412746566f0aad7b
PARK JUWON <jenye4812@gmail.com>
2026-09-17 11:24:13 +0900

    Delete lib directory

--- lib/config/api_config.dart
+++
@@ -1,7 +0,0 @@
-class ApiConfig {
-  static const String baseUrl =
-      "https://dasibom-m9c3.onrender.com/dasibom";
-
-  static const String mediaBaseUrl =
-      "https://dasibom-m9c3.onrender.com";
-}
--- lib/config/route_observer.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-final RouteObserver<PageRoute<dynamic>> routeObserver =
-    RouteObserver<PageRoute<dynamic>>();
